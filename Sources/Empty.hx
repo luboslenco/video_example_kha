1712b424bc07d1162bc79186ee1bdda3e92487c9
luboslenco <lubos.lenco@gmail.com>
2019-01-22 08:22:07 +0100

    Wait for mouse

--- Sources/Empty.hx
+++ Sources/Empty.hx
@@ -5,6 +5,7 @@ import kha.Scheduler;
 import kha.System;
 import kha.Assets;
 import kha.Video;
+import kha.input.Mouse;
 
 class Empty {
 	
@@ -16,18 +17,18 @@ class Empty {
 	
 	function loadFinished() {
 		video = kha.Assets.videos.movie;
+
+		Mouse.get(0).notify(mouseDown, null, null, null, null);	
+		System.notifyOnFrames(render);
+	}
+
+	function mouseDown(button: Int, x: Int, y: Int) {
 		video.play(true);
-		
-		System.notifyOnRender(render);
-		Scheduler.addTimeTask(update, 0, 1 / 60);
+		Mouse.get(0).remove(mouseDown, null, null, null, null);
 	}
 
-	function update(): Void {
-		
-	}
-
-	function render(framebuffer: Framebuffer): Void {
-		var g2 = framebuffer.g2;
+	function render(frames: Array<Framebuffer>): Void {
+		var g2 = frames[0].g2;
 		g2.drawVideo(video, 0, 0, video.width(), video.height());
 	}
 }
--- Sources/Main.hx
+++ Sources/Main.hx
@@ -4,7 +4,7 @@ import kha.System;
 
 class Main {
 	public static function main() {
-		System.init({title: "Empty", width: 800, height: 600}, function() {
+		System.start({title: "Empty", width: 800, height: 600}, function(win: kha.Window) {
 			new Empty();
 		});
 	}
